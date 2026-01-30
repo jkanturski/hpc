ad22017a188608feff42aa079edcdf4bf3bf14eb
jkanturski <54432291+jkanturski@users.noreply.github.com>
2026-01-30 10:51:29 +0100

    Update t2_job.sh

--- t2_job.sh
+++ t2_job.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
-#BSUB -J cifar10_cpu
+#BSUB -J torch_cpu
 #BSUB -q short
 #BSUB -n 4
 #BSUB -R "span[hosts=1]"
-#BSUB -R "rusage[mem=8GB]"
-#BSUB -W 01:00
-#BSUB -o cifar10_%J.out
-#BSUB -e cifar10_%J.err
+#BSUB -W 00:30
+#BSUB -o torch_%J.out
+#BSUB -e torch_%J.err
 
 source ~/miniforge3/etc/profile.d/conda.sh
 conda activate api_llm
 
-# Prevent oversubscription
 export OMP_NUM_THREADS=4
 export MKL_NUM_THREADS=4
-export OPENBLAS_NUM_THREADS=4
-export NUMEXPR_NUM_THREADS=4
 export TORCH_NUM_THREADS=4
 
 python t2.py
